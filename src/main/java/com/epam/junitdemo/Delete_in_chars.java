3df21c22b830ed51600068e378fbeb830e27593c
reshmaShaik0620 <reshmashaik2006@gmail.com>
2020-03-25 19:25:07 +0530

    My first commit

---
+++ src/main/java/com/epam/junitdemo/Delete_in_chars.java
@@ -0,0 +1,19 @@
+package com.epam.junitdemo;
+
+public class Delete_in_chars {
+		public String remove_A_FromFirstTwoChars(String string) {
+			 String str="";
+			if(string.length()>=2 && string.charAt(0)=='A' && string.charAt(1)=='A')
+			str=string.substring(2,string.length());
+			else if(string.length()>=1 && string.charAt(0)=='A')
+				str=string.substring(1,string.length());
+			else if(string.length()>=2 && string.charAt(1)=='A')
+				str=string.charAt(0)+string.substring(2,string.length());
+			else
+				str=string;
+			return str;
+		}
+	}
+	  
+
+
